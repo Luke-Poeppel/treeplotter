c1978225f4c777b06e2645949dc8c0dd8382d0d7
Luke Poeppel <luke.poeppel@gmail.com>
2021-06-10 12:55:56 -0400

    Install zsh syntax bugfix.

--- treeplotter/package_installer.zsh
+++ treeplotter/package_installer.zsh
@@ -25,9 +25,23 @@ function install_webshot {
     Rscript -e 'install.packages("webshot", repos="https://cloud.r-project.org")'
 }
 
-if [ $1 == "force"]
+if [ $1 == "force" ]
 then
     echo Force installing R and the webshot package...
     install_r
     install_webshot
+else
+    echo Would you like to install R? [Y/n]
+    read resp_R
+    if [ $resp_R == "Y" ]
+    then 
+        install_r
+    fi
+    
+    echo Would you like to install the webshot package? [Y/n]
+    read resp_webshot
+    if [ $resp_webshot == "Y" ]
+    then
+        install_webshot
+    fi
 fi
