af795baab5b7f15e6117c904909c888cf467df59
Luke Poeppel <luke.poeppel@gmail.com>
2021-06-10 21:10:24 -0400

    Updated JS file.

--- treeplotter/templates/parse_data.js
+++ treeplotter/templates/parse_data.js
@@ -2,27 +2,12 @@ console.log("Parsing JSON data & Creating Tree...");
 
 require("./Treant.min.js"); 
 var tree_data = require("./tree.json");
-// var chart_config_data_raw = require("./chart_config.json"); 
+var chart_config_data_raw = require("./chart_config.json"); 
 
 var tree = JSON.parse(tree_data);
+var chart_config_data = JSON.parse(chart_config_data_raw); 
 
-// var chart_config_data = JSON.parse(chart_config_data_raw); 
-// var chart_config = {
-//   chart_config_data,
-//   nodeStructure: tree["nodeStructure"]
-// }
-var chart_config = {
-  chart: {
-      container: "#treeplotter",
-      
-      connectors: {
-          type: 'curve'
-      },
-      node: {
-          HTMLclass: "treeNode"
-      }
-  },
-  nodeStructure: tree["nodeStructure"]
-}
+nodeStructureObjDict = tree["nodeStructure"];
+var chart_config = [chart_config_data, nodeStructureObjDict];
 
 new Treant(chart_config);
