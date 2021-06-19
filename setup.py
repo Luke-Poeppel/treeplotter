4e743d4abedb849cbc9489e9918330afd72db916
Luke Poeppel <luke.poeppel@gmail.com>
2021-06-19 14:47:09 -0400

    Bugfix for installation.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os
 
 from setuptools import setup, find_packages
 
-with open(os.path.join("treeplotter", "VERSION")) as version:
+with open(os.path.join(os.path.dirname(__file__), "treeplotter", "VERSION")) as version:
 	__version__ = version.readline()
 
 with open('README.md') as f:
